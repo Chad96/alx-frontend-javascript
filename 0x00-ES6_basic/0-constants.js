74bf0eaed6f14a1767f8d988685f69023fe2632d
Chad96 <chadrackndalamba@gmail.com>
2024-06-24 16:09:21 +0200

    task0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,19 +2,20 @@
 
 // Use const to declare the task variable
 export function taskFirst() {
-  const task = "I prefer const when I can.";
-  return task;
-}
-
-// Import the getLast function
-export function getLast() {
-  return " is okay";
-}
-
-// Use let to declare the combination variable
-export function taskNext() {
-  let combination = "But sometimes let";
-  combination += getLast();
-
-  return combination;
-}
+    const task = 'I prefer const when I can.';
+    return task;
+  }
+  
+  // Define the getLast function
+  export function getLast() {
+    return ' is okay';
+  }
+  
+  // Use let to declare the combination variable
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
+  
